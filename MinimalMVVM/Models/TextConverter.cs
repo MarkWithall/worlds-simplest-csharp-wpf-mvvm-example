693078dbd3008bea98377b19d05db28bb9957ab2
Mark Withall <github@markwithall.com>
2016-10-14 08:49:43 +0100

    Move Model namespace to Models.

--- MinimalMVVM/Models/TextConverter.cs
+++ MinimalMVVM/Models/TextConverter.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace MinimalMVVM.Model
+namespace MinimalMVVM.Models
 {
     public class TextConverter
     {
--- MinimalMVVM/ViewModel/Presenter.cs
+++ MinimalMVVM/ViewModel/Presenter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
-using MinimalMVVM.Model;
+using MinimalMVVM.Models;
 
 namespace MinimalMVVM.ViewModel
 {
