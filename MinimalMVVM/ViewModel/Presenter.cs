c16207cebf953f797c32c01c9a8f76a2c612bb4d
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2018-08-06 10:30:10 -0400

    Use auto-implemented property for backing field

--- MinimalMVVM/ViewModel/Presenter.cs
+++ MinimalMVVM/ViewModel/Presenter.cs
@@ -10,7 +10,7 @@ namespace MinimalMVVM.ViewModel
     {
         private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
         private string _someText;
-        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+        public ObservableCollection<string> History { get; } = new ObservableCollection<string>();
 
         public string SomeText
         {
@@ -22,8 +22,6 @@ namespace MinimalMVVM.ViewModel
             }
         }
 
-        public IEnumerable<string> History => _history;
-
         public ICommand ConvertTextCommand => new DelegateCommand(() =>
         {
             if (string.IsNullOrWhiteSpace(SomeText)) return;
@@ -33,8 +31,8 @@ namespace MinimalMVVM.ViewModel
 
         private void AddToHistory(string item)
         {
-            if (!_history.Contains(item))
-                _history.Add(item);
+            if (!History.Contains(item))
+                History.Add(item);
         }
     }
 }
