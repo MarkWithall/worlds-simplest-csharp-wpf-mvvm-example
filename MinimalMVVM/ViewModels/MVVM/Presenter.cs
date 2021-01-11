295c5eb9864e71f94b2039b54d2434b12c999954
Mark Withall <mark.withall@tracsis.com>
2021-01-11 10:55:56 +0000

    Tidy presenter.

--- MinimalMVVM/ViewModels/MVVM/Presenter.cs
+++ MinimalMVVM/ViewModels/MVVM/Presenter.cs
@@ -10,16 +10,14 @@ namespace MinimalMVVM.ViewModels.MVVM
 
         protected void Update<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
         {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return;
-
-            field = value;
-            OnPropertyChanged(propertyName);
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propertyName);
+            }
         }
 
-        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            var handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
